acbcf6a53d903d96a910b7cdf5e078b0e793eefe
Swaraj Adhikari <swaraj2065@gmail.com>
2026-08-17 02:20:49 -0400

    wrote the tokenizer and parser, parser uses a triple of recursive functions, still many bugs to fix.

--- main.cpp
+++ main.cpp
@@ -9,16 +9,53 @@
 using namespace std;
 using MathFunction = std::function<int(int, int)>;
 
-map<string, int> ascii;
-vector<string> lines;
-map<string, string> type;
-
-map<string, function<int(int, int)>> funcMap = {
-    {"+", [](int a, int b) { return a + b; }},
-    {"-", [](int a, int b) { return a - b; }},
-    {"*", [](int a, int b) { return a * b; }},
-    {"/", [](int a, int b) { return a / b; }},
+enum class TokenKind {
+  Number,
+  Plus,
+  Minus,
+  Star,
+  Slash,
+  LParen,
+  RParen,
+  Eol,
+  Eof
 };
+
+struct Token {
+  TokenKind kind;
+  int value;
+};
+
+map<TokenKind, function<int(int, int)>> funcMap = {
+    {TokenKind::Plus, [](int a, int b) { return a + b; }},
+    {TokenKind::Minus, [](int a, int b) { return a - b; }},
+    {TokenKind::Star, [](int a, int b) { return a * b; }},
+    {TokenKind::Slash, [](int a, int b) { return a / b; }},
+};
+
+std::ostream &operator<<(std::ostream &os, TokenKind kind) {
+  switch (kind) {
+  case TokenKind::Number:
+    return os << "<num>";
+  case TokenKind::Plus:
+    return os << "+";
+  case TokenKind::Minus:
+    return os << "-";
+  case TokenKind::Star:
+    return os << "*";
+  case TokenKind::Slash:
+    return os << "/";
+  case TokenKind::LParen:
+    return os << "(";
+  case TokenKind::RParen:
+    return os << ")";
+  case TokenKind::Eol:
+    return os << "\\n";
+  case TokenKind::Eof:
+    return os << "<eof>";
+  }
+  return os << "??";
+}
 
 class Node {
 public:
@@ -36,82 +73,178 @@ public:
 
 class OpNode : public Node {
 public:
-  OpNode(string operation, unique_ptr<Node> l, unique_ptr<Node> r) {
-    op = operation;
+  OpNode(TokenKind op, unique_ptr<Node> l, unique_ptr<Node> r) {
+    operation = op;
     leftChild = std::move(l);
     rightChild = std::move(r);
   }
-  string op;
+  TokenKind operation;
   unique_ptr<Node> leftChild;
   unique_ptr<Node> rightChild;
 
   int eval() override {
-    return funcMap.at(op)(leftChild->eval(), rightChild->eval());
+    return funcMap.at(operation)(leftChild->eval(), rightChild->eval());
   }
 };
 
-void ascii_mappings(map<string, int> &ascii) {
-  for (int i = 0; i < 10; i++) {
-    ascii.insert({string(1, i + '0'), i + 48});
+vector<Token> tokenizer(string &line) {
+
+  vector<Token> tokens;
+
+  for (int i = 0; i < line.size(); i++) {
+
+    if (line[i] == ' ')
+      continue;
+
+    if (line[i] == '+') {
+      tokens.push_back({TokenKind::Plus, 1});
+      continue;
+    }
+    if (line[i] == '-') {
+      tokens.push_back({TokenKind::Minus, 2});
+      continue;
+    }
+    if (line[i] == '*') {
+      tokens.push_back({TokenKind::Star, 3});
+      continue;
+    }
+    if (line[i] == '/') {
+      tokens.push_back({TokenKind::Slash, 4});
+      i++;
+      continue;
+    }
+    if (line[i] == '(') {
+      tokens.push_back({TokenKind::LParen, 5});
+      continue;
+    }
+    if (line[i] == ')') {
+      tokens.push_back({TokenKind::RParen, 6});
+      continue;
+    }
+
+    if (isdigit(line[i])) {
+      int currentNum = 0;
+      while (i < line.size() && isdigit(line[i])) {
+        currentNum = currentNum * 10 + stoi(string(1, line[i]));
+        i++;
+      }
+      tokens.push_back({TokenKind::Number, currentNum});
+      i--;
+    }
   }
-  ascii.insert({"+", 43});
-  ascii.insert({"-", 45});
-  ascii.insert({"*", 42});
-  ascii.insert({"/", 47});
-
-  ascii.insert({"(", 47});
-  ascii.insert({")", 47});
-
-  return;
+  return tokens;
 }
 
-void create_types(map<string, string> &type) {
-  for (int i = 0; i < 10; i++) {
-    type.insert({string(1, i + '0'), "number"});
+unique_ptr<Node> plusMinus(vector<Token> tokens);
+unique_ptr<Node> multDivide(vector<Token> tokens);
+unique_ptr<Node> parenValue(vector<Token> tokens);
+
+unique_ptr<Node> plusMinus(vector<Token> tokens) {
+  int location = -1;
+
+  for (int i = 0; i < tokens.size(); i++) {
+    if (tokens[i].kind == TokenKind::Plus ||
+        tokens[i].kind == TokenKind::Minus) {
+      location = i;
+      break;
+    }
+    if (tokens[i].kind == TokenKind::LParen) {
+      while (tokens[i].kind != TokenKind::RParen && i < tokens.size()) {
+        i++;
+      }
+    }
+  }
+  if (location == -1) {
+    return multDivide(tokens);
   }
 
-  type.insert({"+", "operator"});
-  type.insert({"-", "operator"});
-  type.insert({"*", "operator"});
-  type.insert({"/", "operator"});
+  vector<Token> tokensLeft;
+  vector<Token> tokensRight;
 
-  type.insert({"(", "parenthesis_open"});
-  type.insert({")", "parenthesis_close"});
-}
+  for (int i = 0; i < location; i++) {
+    tokensLeft.push_back(tokens[i]);
+  }
+  for (int i = location + 1; i < tokens.size(); i++) {
+    tokensRight.push_back(tokens[i]);
+  }
+
+  unique_ptr<Node> left(plusMinus(tokensLeft));
+  unique_ptr<Node> right(plusMinus(tokensRight));
+
+  unique_ptr<Node> root = make_unique<OpNode>(
+      tokens[location].kind, std::move(left), std::move(right));
+  return root;
+};
+
+unique_ptr<Node> multDivide(vector<Token> tokens) {
+  int location = -1;
+
+  for (int i = 0; i < tokens.size(); i++) {
+    if (tokens[i].kind == TokenKind::Star ||
+        tokens[i].kind == TokenKind::Slash) {
+      location = i;
+      break;
+    }
+    if (tokens[i].kind == TokenKind::LParen) {
+      while (tokens[i].kind != TokenKind::RParen && i < tokens.size()) {
+        i++;
+      }
+    }
+  }
+  if (location == -1) {
+    return parenValue(tokens);
+  }
+
+  vector<Token> tokensLeft;
+  vector<Token> tokensRight;
+
+  for (int i = 0; i < location; i++) {
+    tokensLeft.push_back(tokens[i]);
+  }
+  for (int i = location + 1; i < tokens.size(); i++) {
+    tokensRight.push_back(tokens[i]);
+  }
+
+  unique_ptr<Node> left(multDivide(tokensLeft));
+  unique_ptr<Node> right(multDivide(tokensRight));
+
+  unique_ptr<Node> root = make_unique<OpNode>(
+      tokens[location].kind, std::move(left), std::move(right));
+  return root;
+};
+
+unique_ptr<Node> parenValue(vector<Token> tokens) {
+  if (tokens.size() == 1 && tokens[0].kind == TokenKind::Number) {
+    unique_ptr<Node> num = make_unique<NumberNode>(tokens[0].value);
+    return num;
+  }
+  if (tokens[0].kind == TokenKind::LParen &&
+      tokens[tokens.size() - 1].kind == TokenKind::RParen) {
+    vector<Token> newTokens;
+    for (int i = 1; i < tokens.size() - 1; i++) {
+      newTokens.push_back(tokens[i]);
+    }
+    return plusMinus(newTokens);
+  }
+  cout << "PAREN VALUE ERROR\n";
+  for (int i = 0; i < tokens.size(); i++) {
+    cout << tokens[i].kind << ' ';
+  }
+  return nullptr;
+};
 
 int main() {
 
-  ascii_mappings(ascii);
-  create_types(type);
-
+  vector<string> lines;
   string line = "";
 
   while (getline(cin, line)) {
     lines.push_back(line);
   }
 
-  ascii_mappings(ascii);
-
-  create_types(type);
-
-  for (auto &line : lines) {
-    for (auto &chr : line) {
-      if (ascii.count(string(1, chr)) == 0) {
-        cout << "bad characters";
-        return 0;
-      }
-    }
+  for (string line : lines) {
+    vector<Token> tokens = tokenizer(line);
+    auto root = plusMinus(tokens);
+    cout << root->eval();
   }
-
-  unique_ptr<Node> two = make_unique<NumberNode>(2);
-  unique_ptr<Node> three = make_unique<NumberNode>(3);
-  unique_ptr<Node> four = make_unique<NumberNode>(4);
-
-  unique_ptr<Node> addition =
-      make_unique<OpNode>("+", std::move(two), std::move(three));
-
-  unique_ptr<Node> root =
-      make_unique<OpNode>("*", std::move(addition), std::move(four));
-
-  cout << root->eval();
 }
